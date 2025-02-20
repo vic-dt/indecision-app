b1b6b3076168640b35e0f4d22a106e82beaaed25
vic-dt <victoriavr1323@gmail.com>
2025-02-20 19:54:04 +0100

    diferentes modos para crear componentes

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 <template>
     <h1>Mi primera app de Vue</h1>
 <MeusNumerator  :valorem="7" />
-    
+  <MeusNumeratorScript :valorem="10"/>  
 </template>
 
 <script lang="ts" setup>
 import MeusNumerator from './components/MeusNumerator.vue';
 
+import MeusNumeratorScript from './components/MeusNumeratorScript.vue';
 
 
 </script>
