68bedc973dc80717ea472961985f0e30f936c7c5
vic-dt <victoriavr1323@gmail.com>
2025-02-25 20:39:58 +0100

    api yesno

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
-    <div class="flex justify-end">
+
+    <!-- <div class="flex justify-end">
         <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
             Hola, ¿como te va el día?
         </div>
@@ -11,8 +12,15 @@
         <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
             Muy bien, estoy en casa. ¿Y tú?
         </div>
-    </div>
+    </div> -->
 
+            <ChatBullula :meus-est="true" nuntius="¿Salimos a dar una vuelta?"/>
 </div>
 </div>
 </template>
+
+<script lang="ts" setup>
+import ChatBullula from './ChatBullula.vue';
+
+
+</script>
